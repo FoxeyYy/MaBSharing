6881c2e630010e63f2074c604a54896b7d336f48
FoxeyYy <hectorcampop@outlook.com>
2018-03-02 11:02:54 +0100

    Implemented login component logic to use auth guard

--- MaBFrontEnd/src/app/login/login.component.ts
+++ MaBFrontEnd/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router, ActivatedRoute } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,27 +11,32 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
+
   errMsg: string;
-  returnUrl: string;
   loading: boolean;
 
   constructor(
-    private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  login () {
+  login() {
     this.errMsg = '';
 
     if (this.username && this.password) {
       this.loading = true;
-      this.authService.login(this.username, this.password);
-      this.router.navigate([this.returnUrl]);
+      this.authService.login(this.username, this.password)
+        .subscribe(msg => {
+          this.loading = false;
+          if (msg === "error") {
+            this.errMsg = 'Wrong credentials';
+          } else {
+            this.router.navigate(["/"]);
+          }
+        });
     } else {
       this.errMsg = 'Both fields are mandatory';
     }
