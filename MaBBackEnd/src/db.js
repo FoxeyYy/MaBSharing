b7b1cd4d99bd49af6fc2025ca67390dc0adeb538
Raúl Fernández Álvarez <funny.it.worked.last.time@runbox.com>
2018-03-07 17:35:08 +0100

    Fix Log In Not Regsitered Users

--- MaBBackEnd/src/db.js
+++ MaBBackEnd/src/db.js
@@ -60,7 +60,14 @@ const fetchUser = (email) =>
         then(
             rows =>
             {
-                return rows[0];
+                if (rows.length == 0)
+                {
+                    throw new Error('Invalid credentials.');
+                }
+                else
+                {
+                    return rows[0];
+                }
             })
 };
 
