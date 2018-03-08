c1cf4e0129dbfce56af2a688e2bd657a4e09a325
Raúl Fernández Álvarez <funny.it.worked.last.time@runbox.com>
2018-03-08 16:16:35 +0100

    Add DB Poblation Script

--- MaBBackEnd/initdb.d/01-init.sql
+++ MaBBackEnd/initdb.d/01-init.sql
@@ -2,42 +2,38 @@
 CREATE TABLE IF NOT EXISTS user (
   id int(5) NOT NULL AUTO_INCREMENT,
   email varchar(64) NOT NULL UNIQUE CHECK (LENGTH(email) > 0),
-  password varchar(64) NOT NULL,
+  password char(64) NOT NULL,
   creationDate DATE NOT NULL,
   PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
-
 /* Resources table */
 CREATE TABLE IF NOT EXISTS resources (
   id int(5) NOT NULL AUTO_INCREMENT,
-  name varchar(10) NOT NULL,
+  name varchar(30) NOT NULL,
   creationDate DATE NOT NULL,
   releaseDate DATE NOT NULL,
   author_id int(5) NOT NULL,
   PRIMARY KEY(id),
   INDEX author_ind (author_id),
-  FOREIGN KEY (author_id)
-    REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=INNODB;
-
-/* Book table */
-CREATE TABLE IF NOT EXISTS book (
-  id int(5) NOT NULL AUTO_INCREMENT,
-  edition int(5) NOT NULL,
-  writer varchar(30) NOT NULL,
-  resource_id int(5) NOT NULL,
-  PRIMARY KEY(id),
-  INDEX resource_ind (resource_id),
-  FOREIGN KEY (resource_id) REFERENCES resources(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (author_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=INNODB;
 
 /* Movie table */
 CREATE TABLE IF NOT EXISTS movie (
-  id int(5) NOT NULL AUTO_INCREMENT,
   director varchar(30) NOT NULL,
   resource_id int(5) NOT NULL,
-  PRIMARY KEY(id),
+  PRIMARY KEY(resource_id),
+  INDEX resource_ind (resource_id),
+  FOREIGN KEY (resource_id) REFERENCES resources(id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=INNODB;
+
+/* Book table */
+CREATE TABLE IF NOT EXISTS book (
+  edition int(5) NOT NULL,
+  writer varchar(30) NOT NULL,
+  resource_id int(5) NOT NULL,
+  PRIMARY KEY(resource_id),
   INDEX resource_ind (resource_id),
   FOREIGN KEY (resource_id) REFERENCES resources(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=INNODB;
---
+++ MaBBackEnd/initdb.d/02-poblation.sql
@@ -0,0 +1,35 @@
+INSERT INTO user (email, password, creationDate) 
+VALUES 
+    ('hector', 'asd', '2018-03-07'),
+    ('raul', 'asd', '2018-03-07'),
+    ('javier', 'asd', '2018-03-07'),
+    ('cesar', 'asd', '2018-03-07'),
+    ('senmao', 'asd', '2018-03-07'),
+    ('michal', 'asd', '2018-03-07');
+
+INSERT INTO resources (name, creationDate, releaseDate, author_id)
+VALUES 
+    ( 'Blade Runnner 2049', '2018-03-07', '2017-10-06', 1),
+    ( 'Inception', '2018-03-07', '2010-07-16', 4),
+    ( 'Citizen Ken', '2018-03-07', '1941-09-04', 3),
+    ( 'The Godfather', '2018-03-07', '1972-03-11', 2),
+    ( 'Moonlight', '2018-03-07', '2016-10-21', 4),
+    ( 'Manchester by the sea', '2018-03-07', '2016-11-18', 1),
+    ( 'True Fiction', '2018-03-07', '2018-04-01', 5),
+    ( 'Hell''s Princess', '2018-03-07', '2018-04-01', 6),
+    ( 'The lord of the rings', '2018-03-07', '1954-07-24', 6);
+
+INSERT INTO movie
+VALUES
+    ('Denis Villeneuve', 1),
+    ('Christopher Nolan', 2),
+    ('Orson Welles', 3),
+    ('Francis Ford Coppola', 4),
+    ('Barry Jenkins', 5),
+    ('Kenneth Lonergan', 6);
+
+INSERT INTO book 
+VALUES 
+    (3, 'Lee Goldberg', 7),
+    (5, 'Harold Schechter', 8),
+    (1, 'J. R. R. Tolkien', 9);
