2806b1ff876b1303e215f8ec30474d095f0a9f0a
FoxeyYy <hectorcampop@outlook.com>
2018-03-13 14:04:26 +0100

    Search bar hides results if not focused

--- MaBFrontEnd/src/app/navbar/navbar.component.ts
+++ MaBFrontEnd/src/app/navbar/navbar.component.ts
@@ -22,6 +22,7 @@ export class NavbarComponent implements OnInit {
   private results$: Observable<User[] | Book[] | Movie[]>;
   private searchTerms = new Subject<string>();
   private static readonly SEARCH_MIN = 4;
+  private searchBarFocused: boolean = false;
 
   constructor(
     private router: Router,
